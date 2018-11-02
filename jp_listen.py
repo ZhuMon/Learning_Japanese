6d8116e47d2639190422ee3274795ff8dd0f82cc
YuXiang <richardlin0212@gmail.com>
2018-11-02 18:39:47 +0800

    Modularize jp_listen.py

--- jp_listen.py
+++ jp_listen.py
@@ -4,12 +4,14 @@ import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-chromedriver = "/Users/linyuxiang/Desktop/code/Learning_Japanese/chromedriver"
-driver = webdriver.Chrome(chromedriver)
-driver.get("https://translate.google.com.tw/")
+def openweb():
+    chromedriver = "/Users/linyuxiang/Desktop/code/Learning_Japanese/chromedriver"
+    driver = webdriver.Chrome(chromedriver)
+    driver.get("https://translate.google.com.tw/")
+    return driver
 
-#input data
-katakana = [
+def inputdata():
+    katakana = [
 	
 	"コーヒー", "スーツ", "スカート","ユーロ" 
 	, "ソファー", "フィルム", "チェック", "フォード", "ファミリ"  
@@ -38,39 +40,44 @@ katakana = [
 	, "ガム", "ズボン", "ボール", "ゲーム", "ゴルフ"
 	, "ボタン", "チャンネル", "ジーンズ", "ニューヨーク", "ヨーグルト"
 	, "ヨーロッパ", "パイロット", "ロッカー"]
+    return katakana
 
-
-# for many time
-
-#random get data
-
-#input to "text"
-for i in range(0, len(katakana)-1):
+def speak(driver, data, again):
     la_ja = driver.find_element_by_id("sugg-item-ja")
     la_ja.click()
     word = driver.find_element_by_name("text") ##輸入要翻譯的
-    word.send_keys(katakana[random.randint(0, len(katakana))]);
+    word.send_keys(data);
     #word.send_keys(katakana[i]);
     ActionChains(driver).send_keys(Keys.ENTER).perform()
     time.sleep(1)
     element = driver.find_element_by_id("gt-src-listen")
     element.click()
-    for i in range(0,3):
-        a = input("input a to listen again:")
-        if a == "a":
-            element.click()
-            a = " "
-        else:
-            break
+
+    if again:
+        for i in range(0,3):
+            a = input("input a to listen again:")
+            if a == "a":
+                element.click()
+                a = " "
+            else:
+                break
 
 
     clear = driver.find_element_by_id("gt-clear")
     clear.click()
     
+
+if __name__ == '__main__':
+    driver = openweb()
+    data = inputdata()
+    for i in range(0, len(data)-1):
+        data_to_speak = data[random.randint(0, len(data))]
+        speak(driver, data_to_speak, True)
+
 #ActionChain for input
 
 
 #clean
-close()
-quit()
+#close()
+#quit()
 #goto for
