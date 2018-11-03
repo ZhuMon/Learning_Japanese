b8417c2a415c3391e8b1d32dbdfbaded354f39ae
YuXiang <richardlin0212@gmail.com>
2018-11-04 01:44:42 +0800

    Add essay

--- remember_word.py
+++ remember_word.py
@@ -18,13 +18,14 @@ for i in range(0, len(line_list)):
 mode = input("Which mode? (選擇題(a) 問答題(b)) ")
 q_num = input("How many questions? ") # question number
 
+driver = jp_listen.openweb()
+
 if mode is 'a':
     t_num = 0; # true number
     wrong_ans = []
 
-    driver = jp_listen.openweb()
 
-    for i in range(0, q_num):
+    for i in range(0, int(q_num)):
         num = random.randint(0, len(all_word_list)-1)
         wrong1 = random.randint(0, len(all_word_list)-1)
         while wrong1 == num:
@@ -46,52 +47,85 @@ if mode is 'a':
         print(str(i+1)+'. '+ all_word_list[num][ch_or_jp])
         ans = -1
         which_opt = random.randint(0, 3)
-        print("(1) " + all_word_list[opt.pop(which_opt)][another]+' ')
+        print("   (1) " + all_word_list[opt.pop(which_opt)][another]+' ')
         if ans == -1 and which_opt == 0:
             ans = 0
 
         which_opt = random.randint(0, 2)
-        print("(2) " + all_word_list[opt.pop(which_opt)][another]+' ')
+        print("   (2) " + all_word_list[opt.pop(which_opt)][another]+' ')
         if ans == -1 and which_opt == 0:
             ans = 1
 
         which_opt = random.randint(0, 1)
-        print("(3) " + all_word_list[opt.pop(which_opt)][another]+' ')
+        print("   (3) " + all_word_list[opt.pop(which_opt)][another]+' ')
         if ans == -1 and which_opt == 0:
             ans = 2
 
         which_opt = 0
-        print("(4) " + all_word_list[opt.pop()][another]+'\n')
+        print("   (4) " + all_word_list[opt.pop()][another]+'\n')
         if ans == -1 and which_opt == 0:
             ans = 3
         
-        feedback = input()
+        feedback = input("   ")
         if int(feedback)-1 == ans:
             t_num = t_num + 1
-            print("True")
+            print("\n   True")
             jp_listen.speak(driver, all_word_list[num][0], False)
             print("----------------")
         else:
-            print("False")
-            print("Ans is " + all_word_list[num][another])
+            print("\n   False")
+            print("   Ans is " + all_word_list[num][another])
             jp_listen.speak(driver, all_word_list[num][0], False)
             wrong_ans.append(num)
             print("----------------")
 
-    print("Score: " + str(t_num) + '/' + str(q_num))
-    if t_num != q_num:
+    print("Score: " + str(t_num) + '/' + q_num)
+    if t_num != int(q_num):
         print("Wrong answer: ")
-        for i in range(0, q_num-t_num):
+        for i in range(0, int(q_num)-t_num):
             num = wrong_ans.pop(0)
             print(all_word_list[num][0] + '\t' + all_word_list[num][1])
-    driver.quit()
 
 elif mode is 'b':
-    for i in range(0, 10):
-        num = random.randint(0, len(all_word_list)-1)
-        print(all_word_list[num][0])
-        input()
-        print(all_word_list[num][1])
-        input()
+    wk_mode = input("Write or key in? (write(w) key in(k)) ")
+    if wk_mode is 'w':
+        for i in range(0, int(q_num)-1 ):
+            num = random.randint(0, len(all_word_list)-1)
+            ch_or_jp = random.randint(0, 1)
+            another  = 1 - ch_or_jp
+            
+            print(str(i+1) + ". " + all_word_list[num][ch_or_jp])
+            input()
+            print(all_word_list[num][another])
+            jp_listen.speak(driver, all_word_list[num][0], False)
+            print("----------------")
+
+    elif wk_mode is 'k':
+        t_num = 0
+        wrong_ans = []
+        for i in range(0, int(q_num) ):
+            num = random.randint(0, len(all_word_list)-1)
+            print(str(i+1) + ". " + all_word_list[num][1])
+            feedback = input("   日文： ")
+            if feedback == all_word_list[num][0]:
+                print("\n   True")
+                t_num = t_num + 1
+            else:
+                print("\n   False")
+                wrong_ans.append(num)
+                print("   Ans : " + all_word_list[num][0])
+            
+            jp_listen.speak(driver, all_word_list[num][0], False)
+            print("----------------")
+
+        print("Score: " + str(t_num) + '/' + q_num)
+        if t_num != int(q_num):
+            print("Wrong answer: ")
+            for i in range(0, int(q_num)-t_num):
+                num = wrong_ans.pop(0)
+                print(all_word_list[num][0] + '\t' + all_word_list[num][1])
+            
 
 
+
+driver.quit()
