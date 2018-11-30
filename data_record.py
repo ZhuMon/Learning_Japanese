34b5ec2d9218affd87221772682dd6764a302156
YuXiang <richardlin0212@gmail.com>
2018-11-30 23:38:22 +0800

    Store phrase in json

--- data_record.py
+++ data_record.py
@@ -8,7 +8,7 @@ if kind is 'w':
     if len(sys.argv) == 2:
         file = open(sys.argv[1], 'a')
     else:
-        file = open('words', 'a')
+        file = open('json_words', 'a')
     
     print("日文\t中文\t漢字")
 
@@ -27,10 +27,22 @@ elif kind is 'p':
     if len(sys.argv) == 2:
         file = open(sys.argv[1], 'a')
     else:
-        file = open('phrases', 'a')
+        file = open('json_phrases', 'a')
 
-    print("片語（無漢字） 中文 片語（有漢字）")
+    print("片語（無漢字） 中文 有無漢字 片語（有漢字）有無對應句 對應句 對應句有無漢字 對應句（有漢字）")
 
     for i in range(0, int(num)):
         in_data = input()
-        file.write(in_data+'\n')
+        in_list = in_data.split();
+        mydict = {}
+        mydict['jp'] = in_list[0]
+        mydict['ch'] = in_list[1]
+        mydict['ks_bool'] = in_list[2]
+        mydict['ks'] = in_list[3]
+        mydict['aw_bool'] = in_list[4]
+        mydict['aw_jp'] = in_list[5]
+        mydict['aw_ks_bool'] = in_list[6]
+        mydict['aw_ks'] = in_list[7]
+        myjson = json.dumps(mydict)
+        file.write(myjson)
+        file.write('\n')
