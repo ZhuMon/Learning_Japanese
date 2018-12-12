bb879d745e4d091ba3ea989a15f207ccfdea6511
YuXiang <richardlin0212@gmail.com>
2018-12-12 23:36:35 +0800

    Update google translate API

--- jp_listen.py
+++ jp_listen.py
@@ -3,10 +3,15 @@ import random
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+import os
+
+user_data_dir = os.environ['CHROME_USER_DATA']
+driver_path = os.environ['CHROME_DRIVER_PATH']
 
 def openweb():
-    chromedriver = "/Users/linyuxiang/Desktop/code/Learning_Japanese/chromedriver"
-    driver = webdriver.Chrome(chromedriver)
+    dri_Opt = webdriver.ChromeOptions()
+    dri_Opt.add_argument(r"--user-data-dir="+user_data_dir)
+    driver = webdriver.Chrome(driver_path, 0, dri_Opt)
     driver.get("https://translate.google.com.tw/")
     return driver
 
@@ -45,27 +50,33 @@ def inputdata():
 def speak(driver, data, again, sleep_time):
     la_ja = driver.find_element_by_id("sugg-item-ja")
     la_ja.click()
-    word = driver.find_element_by_name("text") ##輸入要翻譯的
+    word = driver.find_element_by_id("source") ##輸入要翻譯的
+    #word.clear()
     word.send_keys(data);
+
     #word.send_keys(katakana[i]);
     ActionChains(driver).send_keys(Keys.ENTER).perform()
     time.sleep(1)
-    element = driver.find_element_by_id("gt-src-listen")
-    element.click()
+    #element = driver.find_element_by_id("btnPlay")
+    #element.click()
+    ActionChains(driver).key_down(Keys.LEFT_CONTROL).key_down(Keys.SHIFT).send_keys('l').perform()
+    ActionChains(driver).key_up(Keys.LEFT_CONTROL).key_up(Keys.SHIFT).perform()
+
     time.sleep(sleep_time)
 
     if again:
         for i in range(0,3):
             a = input("input a to listen again:")
             if a == "a":
-                element.click()
+                ActionChains(driver).key_down(Keys.LEFT_CONTROL).key_down(Keys.SHIFT).send_keys('l').perform()
+                ActionChains(driver).key_up(Keys.LEFT_CONTROL).key_up(Keys.SHIFT).perform()
+
                 a = " "
             else:
                 break
 
 
-    clear = driver.find_element_by_id("gt-clear")
-    clear.click()
+    
     
 
 if __name__ == '__main__':
