85b7fc643a07ce58db9d39fc30bce435fcb6e4d0
YuXiang <richardlin0212@gmail.com>
2019-01-06 01:53:24 +0800

    Use postgreSQL

--- remember_word.py
+++ remember_word.py
@@ -3,29 +3,37 @@ import jp_listen
 import ReuseChrome
 import sys
 import json
+import psycopg2
+
+conn = psycopg2.connect("jp_lr", sslmode='require')
+cur = conn.cursor()
 
 word_key = ['jp', 'ch', 'ks', 'n']
 
+"""
 if len(sys.argv) == 2:
     infile = open(sys.argv[1], 'r')
 else:
     infile = open('test', 'r')
     #infile = open('words', 'r')
-
+"""
 """
 word_list = []
 line_list = []
 all_word_list = []
 """
 
-all_word_list = []
+cur.execute("SELECT * from "+sys.argv[1])
+
+all_word_list = cur.fetchall()
+"""
 file_data = infile.readline()
 while file_data :
     json_data = json.loads(file_data)
     all_word_list.append(json_data)
     file_data = infile.readline()
 
-
+"""
 """
 content = infile.read()
 line_list = content.splitlines(False)
@@ -61,6 +69,9 @@ if mode is 'a':
 
     for i in range(0, int(q_num)):
         num = random.randint(0, len(all_word_list)-1)
+        while all_word_list[num][3] > 5:
+            num = random.randint(0, len(all_word_list)-1)
+
         wrong1 = random.randint(0, len(all_word_list)-1)
         while wrong1 == num:
             wrong1 = random.randint(0, len(all_word_list)-1)
@@ -78,25 +89,25 @@ if mode is 'a':
         ch_or_jp = random.randint(0, 2)
         another  = 0 if ch_or_jp>0 else 1
 
-        print(str(i+1)+'. '+ all_word_list[num][word_key[ch_or_jp]])
+        print(str(i+1)+'. '+ all_word_list[num][ch_or_jp])
         ans = -1
         which_opt = random.randint(0, 3)
-        print("   (1) " + all_word_list[opt.pop(which_opt)][word_key[another]]+' ')
+        print("   (1) " + all_word_list[opt.pop(which_opt)][another]+' ')
         if ans == -1 and which_opt == 0:
             ans = 0
 
         which_opt = random.randint(0, 2)
-        print("   (2) " + all_word_list[opt.pop(which_opt)][word_key[another]]+' ')
+        print("   (2) " + all_word_list[opt.pop(which_opt)][another]+' ')
         if ans == -1 and which_opt == 0:
             ans = 1
 
         which_opt = random.randint(0, 1)
-        print("   (3) " + all_word_list[opt.pop(which_opt)][word_key[another]]+' ')
+        print("   (3) " + all_word_list[opt.pop(which_opt)][another]+' ')
         if ans == -1 and which_opt == 0:
             ans = 2
 
         which_opt = 0
-        print("   (4) " + all_word_list[opt.pop()][word_key[another]]+'\n')
+        print("   (4) " + all_word_list[opt.pop()][another]+'\n')
         if ans == -1 and which_opt == 0:
             ans = 3
         
@@ -107,39 +118,51 @@ if mode is 'a':
             print("----------------")
         else:
             print("\n   False")
-            print("   Ans is " + all_word_list[num][word_key[another]])
+            print("   Ans is " + all_word_list[num][another])
             wrong_ans.append(num)
             print("----------------")
 
-        if ord(all_word_list[num]['ks'][0]) > 128:#if ks isn't english
-            jp_listen.speak(driver, all_word_list[num]['ks'], False, 2)
+        if ord(all_word_list[num][2][0]) > 128:#if ks isn't english
+            jp_listen.speak(driver, all_word_list[num][2], False, 2)
         else:
-            jp_listen.speak(driver, all_word_list[num]['jp'], False, 2)
+            jp_listen.speak(driver, all_word_list[num][0], False, 2)
 
     print("Score: " + str(t_num) + '/' + q_num)
     if t_num != int(q_num):
         print("Wrong answer: ")
         for i in range(0, int(q_num)-t_num):
             num = wrong_ans.pop(0)
-            print(all_word_list[num]['jp'], '\t', all_word_list[num]['ch'], '\t', all_word_list[num]['ks'])
+            out1 = all_word_list[num][0]
+            if len(out1) > 3:
+                out1 = out1 + '\t'
+            else:
+                out1 = out1 + '\t\t'
+            out2 = all_word_list[num][2]
+            if len(out2) > 3:
+                out2 = out2 + '\t'
+            else:
+                out2 = out2 + '\t\t'
+            print(out1 + out2 + all_word_list[num][1])
 
 elif mode is 'b':
     wk_mode = input("Write or key in? (write(w) key in(k)) ")
     if wk_mode is 'w':
         for i in range(0, int(q_num)-1 ):
             num = random.randint(0, len(all_word_list)-1)
+            while all_word_list[num][3] > 5:
+                num = random.randint(0, len(all_word_list)-1)
             ch_or_jp = random.randint(0, 2)
             another  = 0 if ch_or_jp>0 else 1
             
-            print(str(i+1) + ". " + all_word_list[num][word_key[ch_or_jp]])
+            print(str(i+1) + ". " + all_word_list[num][ch_or_jp])
             input()
-            print(all_word_list[num]['jp'])
-            print(all_word_list[num]['ks'] +'\t' +all_word_list[num]['ch'])
+            print(all_word_list[num][0])
+            print(all_word_list[num][2] +'\t' +all_word_list[num][1])
 
-            if ord(all_word_list[num]['ks'][0]) > 128:#if ks isn't english
-                jp_listen.speak(driver, all_word_list[num]['ks'], False, 2)
+            if ord(all_word_list[num][2][0]) > 128:#if ks isn't english
+                jp_listen.speak(driver, all_word_list[num][2], False, 2)
             else:
-                jp_listen.speak(driver, all_word_list[num]['jp'], False, 2)
+                jp_listen.speak(driver, all_word_list[num][0], False, 2)
             print("----------------")
 
     elif wk_mode is 'k':
@@ -147,42 +170,53 @@ elif mode is 'b':
         wrong_ans = []
         for i in range(0, int(q_num) ):
             num = random.randint(0, len(all_word_list)-1)
+            while all_word_list[num][3] > 5:
+                num = random.randint(0, len(all_word_list)-1)
             ch_or_jp = random.randint(1, 2)
 
-            if ch_or_jp == 2 and all_word_list[num]['jp'] == all_word_list[num]['ks']:
+            if ch_or_jp == 2 and all_word_list[num][0] == all_word_list[num][2]:
                 ch_or_jp = 1
 
             if ch_or_jp == 1:
-                print(str(i+1) + ". " + all_word_list[num]['ch'])
+                print(str(i+1) + ". " + all_word_list[num][1])
                 feedback = input("   日文： ")
-                if feedback == all_word_list[num]['jp'] or feedback == all_word_list[num]['ks']:
+                if feedback == all_word_list[num][0] or feedback == all_word_list[num][2]:
                     print("\n   True")
                     t_num = t_num + 1
+                    cur.execute("UPDATE "+sys.argv[1]+" set num = num + 1 where jp='" + all_word_list[num][0]+"'")
+                    cur.commit()
                 else:
                     print("\n   False")
                     wrong_ans.append(num)
-                    print("   Ans : " + all_word_list[num]['jp'])
+                    print("   Ans : " + all_word_list[num][0])
+                    cur.execute("UPDATE "+sys.argv[1]+" set num = num - 1 where jp='" + all_word_list[num][0]+"'")
+                    cur.commit()
+                    
 
             elif ch_or_jp == 2:
-                print(str(i+1) + ". " + all_word_list[num]['ks'])
+                print(str(i+1) + ". " + all_word_list[num][2])
 
-                if ord(all_word_list[num]['ks'][0]) < 128:
+                if ord(all_word_list[num][2][0]) < 128:
                     feedback = input("   片假名： ")
                 else:
                     feedback = input("   平假名： ")
-                if feedback == all_word_list[num]['jp']:
+                if feedback == all_word_list[num][0]:
                     print("\n   True")
                     t_num = t_num + 1
+                    cur.execute("UPDATE "+sys.argv[1]+" set num = num + 1 where jp='" + all_word_list[num][0]+"'")
+                    cur.commit()
                 else:
                     print("\n   False")
                     wrong_ans.append(num)
-                    print("   Ans : " + all_word_list[num]['jp'])
-                    print("         " + all_word_list[num]['ks'])
+                    print("   Ans : " + all_word_list[num][0])
+                    print("         " + all_word_list[num][2])
+                    cur.execute("UPDATE "+sys.argv[1]+" set num = num - 1 where jp='" + all_word_list[num][0]+"'")
+                    cur.commit()
             
-            if ord(all_word_list[num]['ks'][0]) > 128:#if ks isn't english
-                jp_listen.speak(driver, all_word_list[num]['ks'], False, 2)
+            if ord(all_word_list[num][2][0]) > 128:#if ks isn't english
+                jp_listen.speak(driver, all_word_list[num][2], False, 2)
             else:
-                jp_listen.speak(driver, all_word_list[num]['jp'], False, 2)
+                jp_listen.speak(driver, all_word_list[num][0], False, 2)
             print("----------------")
 
         print("Score: " + str(t_num) + '/' + q_num)
@@ -191,17 +225,17 @@ elif mode is 'b':
             for i in range(0, int(q_num)-t_num):
                 num = wrong_ans.pop(0)
                 
-                out1 = all_word_list[num]['jp']
+                out1 = all_word_list[num][0]
                 if len(out1) > 3:
                     out1 = out1 + '\t'
                 else:
                     out1 = out1 + '\t\t'
-                out2 = all_word_list[num]['ks']
+                out2 = all_word_list[num][2]
                 if len(out2) > 3:
                     out2 = out2 + '\t'
                 else:
                     out2 = out2 + '\t\t'
-                print(out1 + out2 + all_word_list[num]['ch'])
+                print(out1 + out2 + all_word_list[num][1])
             
 
 cont_web = input("Do you continue to learn? Yes(y)/No(n) ")
@@ -214,3 +248,5 @@ if cont_web is 'y':
 else:
     driver.quit()
  
+
+conn.close()
