1e5dd53b075ed12f3ea6770daafef86f4a26ebf2
YuXiang <richardlin0212@gmail.com>
2019-04-15 23:05:34 +0800

    Solve blank problem When record data, data can't include blank. Now can!

--- data_record_db.py
+++ data_record_db.py
@@ -21,7 +21,7 @@ num = input("How many? ")
 
 for i in range(0, int(num)):
     in_data = input()
-    in_list = in_data.split();
+    in_list = in_data.split('\t');
 
     cur.execute("INSERT INTO "+sys.argv[1]+"(jp, ch, ks, num, index) Values ('"+in_list[0]+"', '"+in_list[1]+"', '"+in_list[2]+"', '"+in_list[3]+"', '"+in_list[4]+"');")
     
