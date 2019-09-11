abc419f6b13f8e377df67dbbf7dd507d97048a44
YuXiang <richardlin0212@gmail.com>
2019-09-11 22:22:30 +0800

    Add main && Change time.py

---
+++ __main__.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import MyTime
+
+class MainConsole(Frame):
+    def __init__(self, parent = None):
+        Frame.__init__(self, parent)
+
+
+    def create_menubar(self, level = None):
+        None
+
+    def time(self):
+        MyTime.start()
+
+    def clear_widget(self, level = None):
+        None
+
+    
+
+if __name__ == "__main__":
+    root = Tk()
+    MainConsole(root)
+    
+    root.mainloop()
+
--- time.py
+++ time.py
@@ -1,31 +1,47 @@
 import random
 
-hour_l = ["いち", "に", "さん" , "よ" , "ご" , "ろく" , "しち" , "はち", "く" , "じゅう" , "じゅういち" , "じゅうに"]
-minute_l = [ "いっ", "に", "さん", "よん", "ご", "ろっ", "なな", "はっ", "きゅう", "じゅっ"]
 
-while True:
-    hour = random.randint(1, 12)
-    minute = random.randint(0, 59)
+class MyTime():
+    def __init__(self):
+        self.hour_l = ["いち", "に", "さん" , "よ" , "ご" , "ろく" , "しち" , "はち", "く" , "じゅう" , "じゅういち" , "じゅうに"]
+        self.minute_l = [ "いっ", "に", "さん", "よん", "ご", "ろっ", "なな", "はっ", "きゅう", "じゅっ"]
+        self.state = False
 
-    print(str(hour)+":"+str(minute))
-    input()
+    def start():
+        """ Start function """
+        try:
+            self.state = True
+            while self.state:
+                hour = random.randint(1, 12)
+                minute = random.randint(0, 59)
 
-    out = ""
-    tmp = int(minute/10)
-    if tmp > 1:
-        out = minute_l[tmp-1]
-    if tmp >= 1 and minute%10 != 0:
-        out = out + hour_l[9]
-    
-    tmp = minute%10
-    if tmp == 2 or tmp == 5 or tmp == 7 or tmp == 9:
-        out = out + minute_l[tmp - 1] + " ふん"
-    else:
-        if tmp != 0 :#and int(minute/10) > 1:
-            out = out + minute_l[tmp - 1] + " ぷん"
-        elif tmp == 0:
-            out = out + minute_l[9] + " ぷん"
-            
+                print(str(hour)+":"+str(minute))
+                input()
 
-    
-    print(hour_l[hour-1] + " じ " + out)
+                out = ""
+                tmp = int(minute/10)
+                if tmp > 1:
+                    out = self.minute_l[tmp-1]
+                if tmp >= 1 and minute%10 != 0:
+                    out = out + self.hour_l[9]
+                
+                tmp = minute%10
+                if tmp == 2 or tmp == 5 or tmp == 7 or tmp == 9:
+                    out = out + self.minute_l[tmp - 1] + " ふん"
+                else:
+                    if tmp != 0 :#and int(minute/10) > 1:
+                        out = out + self.minute_l[tmp - 1] + " ぷん"
+                    elif tmp == 0:
+                        out = out + self.minute_l[9] + " ぷん"
+                        
+                print(self.hour_l[hour-1] + " じ " + out)
+
+        except KeyboardInterrupt:
+            self.state = False
+            print("Shutting Down")
+
+    def stop():
+        self.state = False
+
+if __name__ == "__main__":
+    MyTime.start()
