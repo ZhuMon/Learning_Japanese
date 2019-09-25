484cf42be0586d790fa92339cabc6f0b4ee8d57c
YuXiang <richardlin0212@gmail.com>
2019-09-25 17:21:31 +0800

    Add something

--- lib/remember_word.py
+++ lib/remember_word.py
@@ -5,9 +5,14 @@ import sys
 import json
 
 class RememberWord():
-    def __init__(self, num_word = 20):
+    def __init__(self, word_list = None, num_word = 20):
+        """
+        word_list = [obj1, obj2, ...]
+        obj1 = Word()
+        Word.jp Word.ch Word.ks Word.n
+        """
+        self.word_list = word_list
         self.num_word = num_word
-        None
 
     def start(self, mode, q_num, listen):
         if listen == True:
@@ -31,11 +36,31 @@ class RememberWord():
             self.keyin() 
 
     def choose(self, q_num): 
+        """
+        input:
+        q_num : number of questions : type: (int)
+
+        return:
+        {question1:[answer, opt1, opt2, opt3], q2:...} 
+
+        """
         t_num = 0 # true number
         wrong_ans = []
+        out = {}
 
         for i in range(0, int(q_num)):
+            opt = random.sample(range(0, num_word), 4)
+            num = random.sample(opt, 1)  # true answer
+            
+            out[self.word_list[num].jp] = []
 
+            # question is chinese or japanese
+            ch_or_jp = random.randint(0, 2)    
+            another  = 0 if ch_or_jp>0 else 1
+
+            ans = -1
+            which_opt = random.randint(0, 3)
+            
     def keyin(self):
         None
 
